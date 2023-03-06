9ddfcfe4fd2848abf60b76ee5b19482881e81c96
Bilal Ahmad <bilal.ahmad@focusteck.com>
2023-03-06 13:29:14 +0500

    This is commit on 6th march

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import ReactDOM from 'react-dom/client';
+//import reportWebVitals from './reportWebVitals';
 import './App.css';
+import React from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const fullname = "Bilal Ahmad Qureshi";
+  const fname = "Bilal";
+  const mname = "Ahmad";
+  const lname = "Qureshi";
+  const ctime = new Date().toLocaleTimeString();
+  const cdate = new Date().toLocaleDateString();
+  const img1 = "https://picsum.photos/200/300";
+  const img2 = "https://picsum.photos/200/300";
+  const img3 = "https://picsum.photos/200/300";
+  const link1 = "https://reactjs.org/";
 
+  return (
+    <div>
+      <h1 className='heading'>{`My name is ${fname} ${mname} ${lname}`}</h1>,
+      <h2 className='s_heading'>This is my first react test app for training</h2>,
+      <div className='para'>
+        <p>The sum of 2 and 3 is {2 + 3}</p>,
+        <p>{`Time right now is: ${ctime}`}</p>
+        <p>{`Date today is: ${cdate}`}</p>
+        <p>My full name is {fullname}</p>
+      </div>
+
+      <div className='images'>
+        <a href={link1} target="">
+          <img src={img3} alt="Not found" />
+        </a>
+        <img src={img1}
+          alt="Not found" />
+        <img src={img2}
+          alt="Not found" />
+      </div>
+
+      <h3 className='third_heading'>Here are some of my favourite motorbikes in Pakistan</h3>
+      <ol className='list'>
+        <div className='sublist'>
+          <li>Kawasaki Ninja ZX10R</li>
+          <li>Kawasaki Ninja H2R</li>
+          <li>Ducatti Panegale v4</li>
+        </div>
+
+      </ol>
+    </div>
+  )
+
+
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,27 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  [
-    <h1>Hello World</h1>,
-    <p>This is my first react test app</p>,
-    <h2>Hello World 2</h2>
-  ]
+    <App />
 );
 
 
-/* if we want to write it in pure JS, then
-var h1 = document.createElement(h1);
-h1.innerHTML="Hello World";
-document.getElementbyID("root");
-now this work the same way as of: 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<h1>Hello World</h1>
-);
-*/
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
